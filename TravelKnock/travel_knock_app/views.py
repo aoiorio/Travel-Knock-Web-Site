70ae7d6a4e79e2922550f81ac1067bb8157e6b33
aoiorio <130330538+aoiorio@users.noreply.github.com>
2023-12-30 20:41:40 +0900

    🥞 create: home page

--- TravelKnock/travel_knock_app/views.py
+++ TravelKnock/travel_knock_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+# Home Page
+class HomeView(TemplateView):
+    template_name = 'home/home.html'
